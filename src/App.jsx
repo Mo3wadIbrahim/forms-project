de930527e87990b737feb231c3ed0355b11046ba
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-06-19 18:29:42 +0300

    Project Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,17 @@ import Login from "./components/Login.jsx";
 function App() {
   return (
     <>
-      <h1>Welcome to the React App</h1>
+      <h1
+        style={{
+          color: "blue",
+          fontSize: "2rem",
+          fontFamily: "Arial",
+          textAlign: "center",
+          margin: "20px 0",
+        }}
+      >
+        Welcome to the React App
+      </h1>
       <Header />
       <main>
         <Login />
