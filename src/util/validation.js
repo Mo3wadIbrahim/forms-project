12cf91b579df931b93688ceb3ffdfb60e8aac4ae
Moawad Ibrahim <mo3wadibrahim@gmail.com>
2026-06-20 20:33:25 +0300

    Project Updated

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,27 +1,14 @@
-export function isEmail(value) {
-  return value.includes("@");
+export function isEmpty(value) {
+  return value.trim() === "";
 }
-
-export function isNotEmpty(value) {
-  return value.trim() !== "";
+export function isEqual(value1, value2) {
+  return value1 === value2;
 }
-
-export function hasMinLength(value, minLength) {
-  return value.length >= minLength;
-}
-
-export function isEqualsToOtherValue(value, otherValue) {
-  return value === otherValue;
-}
-
 export const isValidEmail = (value) => {
   return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
 };
 export const isValidPassword = (value, length) => {
-  // Construct the regex string dynamically using the length parameter
-  const regexString = `^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{${length},}$`;
-
-  const passwordRegex = new RegExp(regexString);
-
-  return passwordRegex.test(value);
+  return RegExp(
+    `^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{${length},}$`,
+  ).test(value);
 };
